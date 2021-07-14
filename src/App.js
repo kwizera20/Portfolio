e188245b7e0a6a8883ddec8d3d1a168743f2676e
kwizera <kwizeraernest20@gmail.com>
2021-07-14 10:51:45 +0200

    portfolio-project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div>
+      <h2>Hello, world!!!!</h2>
+    </div>
+  );
+}
+
+export default App;
